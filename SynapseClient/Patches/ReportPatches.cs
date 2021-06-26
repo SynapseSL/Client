aa3d5b121e7110693aa22809e6b4ecbe950057c7
GrafDimenzio <crafterwolfhd@gmail.com>
2021-06-26 17:36:16 +0200

    report

---
+++ SynapseClient/Patches/ReportPatches.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HarmonyLib;
+
+namespace SynapseClient.Patches
+{
+    public class ReportPatches
+    {
+        [HarmonyPatch(typeof(CheaterReport),nameof(CheaterReport.CmdReport))]
+        [HarmonyPrefix]
+        public bool OnReport(CheaterReport __instance,int playerId, string reason, bool notifyGm)
+        {
+            if (!notifyGm) return true;
+
+            try
+            {
+                Logger.Info($"Start Report of {playerId} for {reason}");
+            }
+            catch(Exception e)
+            {
+                Logger.Error("Synapse-Report: Global Report failed:\n" + e);
+            }
+            return false;
+        }
+    }
+}
