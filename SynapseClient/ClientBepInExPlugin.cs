4ac9c68b6a954a8c9879e20bce94ee0c44c6c87b
GrafDimenzio <crafterwolfhd@gmail.com>
2021-06-29 13:41:37 +0200

    added Synapse Build Information in BuildInfo command

--- SynapseClient/ClientBepInExPlugin.cs
+++ SynapseClient/ClientBepInExPlugin.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using BepInEx;
 using BepInEx.IL2CPP;
+using CommandSystem.Commands;
 using Newtonsoft.Json;
 using SynapseClient.API;
 using SynapseClient.API.Events;
@@ -15,10 +16,11 @@ namespace SynapseClient
     [BepInPlugin("xyz.synapse.client.plugin", "SynapseClient", "0.0.0.1")]
     public class ClientBepInExPlugin : BasePlugin
     {
-        public const string ClientVersion = "1.0.0";
         public const int ClientMajor = 1;
         public const int ClientMinor = 0;
         public const int ClientPatch = 0;
+        public const string ClientVersion = "1.0.0-Beta";
+        public const string ClientDescription = "The Synapse client is a modificated version of the SCP:SL client with a lot of Features like Custom Central Server support and Packet Pipeline support between Server and Client";
 
         public string CentralServer { get; private set; } = "https://central.synapsesl.xyz";
         public string ServerListServer { get; private set; } = "https://servers.synapsesl.xyz";
@@ -35,6 +37,9 @@ namespace SynapseClient
 
             try
             {
+                CustomNetworkManager.Modded = true;
+                BuildInfoCommand.ModDescription = $"\n=====ClientMod=====\nClient: Synapse\nClient Version: {ClientVersion}\nDescription: {ClientDescription}";
+
                 LoadServersFromJson();
 
                 ModLoader.LoadAll();
