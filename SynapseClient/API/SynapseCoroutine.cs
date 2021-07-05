e415111e9663776792cf32322333b4d360d13311
GrafDimenzio <crafterwolfhd@gmail.com>
2021-07-05 08:15:26 +0200

    added SynapseCoroutine.WaitForSeconds()

--- SynapseClient/API/SynapseCoroutine.cs
+++ SynapseClient/API/SynapseCoroutine.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using MelonLoader.Support;
+using UnityEngine;
 
 namespace SynapseClient.API
 {
@@ -10,6 +11,8 @@ namespace SynapseClient.API
         public static SynapseCoroutine StartCoroutine(IEnumerator enumerator) => new SynapseCoroutine(Coroutines.Start(enumerator));
 
         public static void StopCoroutine(SynapseCoroutine coroutine) => Coroutines.Stop((IEnumerator)coroutine.handle);
+
+        public static WaitForSeconds WaitForSeconds(float time) => new WaitForSeconds(time);
 
         private readonly object handle;
     }
